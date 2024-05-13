21efbd6ef9bdeb0c4dca46a8a1f10a11f98c13be
Sérgio <ribeirosergio>
2024-05-13 15:22:48 -0300

    teste técnico ATG

---
+++ OrderAccumulator/Program.cs
@@ -0,0 +1,125 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using QuickFix.FIX44;
+
+namespace OrderAccumulatorMicroservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SessionSettings settings = new("orderaccumulator.cfg");
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            IMessageFactory messageFactory = new DefaultMessageFactory();
+
+            OrderAccumulatorApplication application = new();
+
+            ThreadedSocketAcceptor acceptor = new(application, storeFactory, settings, logFactory, messageFactory);
+            acceptor.Start();
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar.");
+
+            acceptor.Stop();
+        }
+    }
+
+    class OrderAccumulatorApplication : MessageCracker, IApplication
+    {
+        Session? _session;
+
+        decimal limiteExposicao = 1000000;
+        decimal exposicaoAtual = 0;
+        int orderID = 0;
+        int execID = 0;
+        int origClOrdID = 0;
+
+        private string GenOrderID() { return (++orderID).ToString(); }
+        private string GenExecID() { return (++execID).ToString(); }
+        private string GenOrigClOrdID() { return (++origClOrdID).ToString(); }
+
+        public void FromApp(QuickFix.Message message, SessionID sessionId) { }
+        public void OnCreate(SessionID sessionId) { }
+        public void OnLogon(SessionID sessionId) { _session = Session.LookupSession(sessionId); }
+        public void OnLogout(SessionID sessionId) { }
+        public void ToAdmin(QuickFix.Message message, SessionID sessionId) { }
+        public void ToApp(QuickFix.Message message, SessionID sessionId) { }
+        public void FromAdmin(QuickFix.Message message, SessionID sessionId) { }
+
+        public void OnMessage(NewOrderSingle order, SessionID sessionID)
+        {
+            decimal orderValue = order.OrderQty.getValue() * order.Price.getValue();
+
+            if (order.Side.getValue() == Side.BUY)
+            {
+                if (exposicaoAtual + orderValue <= limiteExposicao)
+                {
+                    exposicaoAtual += orderValue;
+                    ExecutionReport executionReport = new(
+                        new OrderID(GenOrderID()),
+                        new ExecID(GenExecID()), 
+                        new ExecType(ExecType.FILL), 
+                        new OrdStatus(OrdStatus.FILLED), 
+                        order.Symbol,
+                        order.Side, 
+                        new LeavesQty(0),
+                        new CumQty(order.OrderQty.getValue()),
+                        new AvgPx(order.Price.getValue())
+                    );
+
+                    try
+                    {
+                        _session?.Send(executionReport);
+                    }
+                    catch (SessionNotFound ex)
+                    {
+                        Console.WriteLine("Erro ao enviar ExecutionReport: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    OrderCancelReject cancelReject = new(
+                        new OrderID(GenOrderID()),
+                        new ClOrdID(GenExecID()), 
+                        new OrigClOrdID(GenOrigClOrdID()),
+                        new OrdStatus(OrdStatus.REJECTED),
+                        new CxlRejResponseTo(CxlRejResponseTo.ORDER_CANCEL_REQUEST)
+                    );
+
+                    try
+                    {
+                        _session?.Send(cancelReject);
+                    }
+                    catch (SessionNotFound ex)
+                    {
+                        Console.WriteLine("Erro ao enviar OrderCancelReject: " + ex.Message);
+                    }
+                }
+            }
+            else if (order.Side.getValue() == Side.SELL)
+            {
+                exposicaoAtual -= orderValue;
+                ExecutionReport executionReport = new ExecutionReport(
+                    new OrderID(GenOrderID()),
+                    new ExecID(GenExecID()),
+                    new ExecType(ExecType.FILL),
+                    new OrdStatus(OrdStatus.FILLED),
+                    order.Symbol, 
+                    order.Side,
+                    new LeavesQty(0), 
+                    new CumQty(order.OrderQty.getValue()),
+                    new AvgPx(order.Price.getValue()) 
+                );
+
+                try
+                {
+                    _session?.Send(executionReport);
+                }
+                catch (SessionNotFound ex)
+                {
+                    Console.WriteLine("Erro ao enviar ExecutionReport: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ OrderGenerator/Program.cs
@@ -0,0 +1,92 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using QuickFix.FIX44;
+using Message = QuickFix.Message;
+
+namespace OrderGeneratorMicroservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SessionSettings settings = new("ordergenerator.cfg");
+                IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+                ILogFactory logFactory = new FileLogFactory(settings);
+                IMessageFactory messageFactory = new DefaultMessageFactory();
+
+                OrderGeneratorApplication application = new();
+
+                QuickFix.Transport.SocketInitiator initiator = new(application, storeFactory, settings, logFactory, messageFactory);
+                initiator.Start();
+
+                Console.WriteLine("Pressione qualquer tecla para encerrar.");
+                Console.ReadKey();
+
+                initiator.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu uma exceção: " + ex.Message);
+            }
+        }
+    }
+
+    class OrderGeneratorApplication : MessageCracker, IApplication
+    {
+        Session? _session;
+        Random _random = new();
+        string[] _symbols = { "PETR4", "VALE3", "VIIA4" };
+        char[] _sides = { '1', '2' }; // '1' for Buy, '2' for Sell
+        int clOrdID = 0;
+
+        private string GenClOrdID() { return (++clOrdID).ToString(); }
+
+        public void FromApp(Message message, SessionID sessionId) { }
+        public void OnCreate(SessionID sessionId) { }
+        public void OnLogon(SessionID sessionId)
+        {
+            _session = Session.LookupSession(sessionId);
+
+            while (true)
+            {
+                NewOrderSingle newOrder = GenerateNewOrderSingle();
+                SendOrder(newOrder);
+            }
+        }
+        public void OnLogout(SessionID sessionId) { }
+        public void ToAdmin(Message message, SessionID sessionId) { }
+        public void ToApp(Message message, SessionID sessionId) { }
+        public void FromAdmin(Message message, SessionID sessionId) { }
+
+        public static void OnMessage(ExecutionReport report, SessionID sessionID)
+        {
+            Console.WriteLine("ExecutionReport recebida: " + report.ToString());
+        }
+
+        public void SendOrder(NewOrderSingle order)
+        {
+            _session?.Send(order);
+        }
+
+        private NewOrderSingle GenerateNewOrderSingle()
+        {
+            string symbol = _symbols[_random.Next(0, _symbols.Length)];
+            char side = _sides[_random.Next(0, _sides.Length)];
+
+            NewOrderSingle newOrder = new(
+                new ClOrdID(GenClOrdID()), 
+                new Symbol(symbol), 
+                new Side(side), 
+                new TransactTime(DateTime.Now), 
+                new OrdType(OrdType.LIMIT) 
+            );
+
+            newOrder.Set(new OrderQty(_random.Next(1, 100000)));
+            newOrder.Set(new Price((decimal)(_random.Next(1, 100000) / 100.0))); 
+
+            return newOrder;
+        }
+    }
+}
